f2ab1224e88fbe1c6b3c192e1f4569585bf72e22
Suhail1626 <104824273+Suhail1626@users.noreply.github.com>
2022-11-03 15:07:25 +0530

    Add files via upload

---
+++ details.js
@@ -0,0 +1,6 @@
+module.exports = {
+    token: '5778271065:AAF5a-SKuvVPeV0lBv4Ovnq3j-lWnTGCidg',
+    admins: [1714908211],
+    mongo_url: 'mongodb+srv://Suhail:Suhail123@cluster0.vssk8nj.mongodb.net/?retryWrites=true&w=majority',
+}
+
